4575fdb13381382a9c4f3bff6cf7c89c6f086a17
Maarten Wolfsen <maartenwolfsen@gmail.com>
2020-06-30 17:35:36 +0200

    Finish functions implementation

--- src/game.c
+++ src/game.c
@@ -12,7 +12,6 @@
 #pragma comment(lib, "winmm.lib")
 
 #define BUFFER_LENGTH 1024
-
 #define MAX_STRING_CHARS 20
 #define CHOICE_MAX_ATTEMPTS 10
 #define CON_DELAY 500
@@ -88,12 +87,14 @@ int main()
         return 0;
     }
 
-    PlaySound(".\\src\\sound\\song.wav", NULL, SND_ASYNC);
+    //PlaySound(".\\src\\sound\\song.wav", NULL, SND_ASYNC);
 
     int line_size = 1024;
-    char buffer[100];
     int nestLevel = 0;
     int inChoice = 0;
+    char buffer[BUFFER_LENGTH];
+    char name[MAX_STRING_CHARS];
+    char location[MAX_STRING_CHARS];
 
     while (fgets(buffer, BUFFER_LENGTH, filePointer)) {
         char *str = buffer + (4 * nestLevel);
@@ -125,11 +126,12 @@ int main()
                 char *quote = str + (strlen(quoteBuffer) + 2);
                 strrtn(quote);
 
-                printf("%s: \"%s\"\n", quoteBuffer + 2, quote);
+                printnpcc(quoteBuffer + 2, quote);
             }
         }
 
         // Execute Function
+        // TODO: Find better function execution method
         if (*(str + 1) == 'f') {
             char *f1, *f2;
             f1 = strstr(str, TYPE_START) + 1;
@@ -140,7 +142,17 @@ int main()
                 char funcBuffer[128];
                 snprintf(funcBuffer, sizeof(buffer), "%.*s", f2 - f1, f1);
 
-                printf("%s", funcBuffer + 2);
+                char *function = funcBuffer + 2;
+
+                if (strcmp(function, "setName") == 0) {
+                    fgets(name, MAX_STRING_CHARS, stdin);
+                    strrtn(name);
+                    printnpcc(name, name);
+                } else if (strcmp(function, "setLocation") == 0) {
+                    fgets(location, MAX_STRING_CHARS, stdin);
+                    strrtn(location);
+                    printnpcc(location, location);
+                }
             }
         }
 
@@ -163,7 +175,7 @@ int main()
                     char assignmentBuffer[128];
                     char choiceTypes[10];
                     snprintf(assignmentBuffer, sizeof(buffer), "%.*s\n", p2 - p1, p1);
-                    char* choices = strtok(assignmentBuffer, CHOICE_SEPARATOR);
+                    char *choices = strtok(assignmentBuffer, CHOICE_SEPARATOR);
 
                     while (choices != NULL) {
                         printf("[%c] %s\n", toupper(choices[0]), choices + 2);
@@ -196,79 +208,4 @@ int main()
     }
 
     fclose(filePointer);
-    
-    /*
-    char name[MAX_STRING_CHARS];
-    char location[MAX_STRING_CHARS];
-    char choiceAnswer[50];
-
-    // printf(node);
-
-    printsep();
-    printf("  ________            ______                      __         \n");
-    printf(" /_  __/ /_  ___     /_  __/________ __   _____  / /__  _____\n");
-    printf("  / / / __ \\/ _ \\     / / / ___/ __ `/ | / / _ \\/ / _ \\/ ___/\n");
-    printf(" / / / / / /  __/    / / / /  / /_/ /| |/ /  __/ /  __/ /    \n");
-    printf("/_/ /_/ /_/\\___/    /_/ /_/   \\__,_/ |___/\\___/_/\\___/_/     \n");
-    printf("                      A Short Story by Maarten Wolfsen\n\n");
-    printf("---- Main Menu -------------------------------------\n");
-    printf("[A] Start Game.\n");
-    printf("[B] Exit.\n");
-    printsep();
-
-    init_choice();
-
-    if (strcmp(choice, "B") == 0) {
-        printf("Exiting Game...\n");
-        exit(0);
-    } else {
-        printnpcc("Bartender", "Hello weary Traveler. You have been on quite the Journey, what is your name?");
-
-        fgets(name, MAX_STRING_CHARS, stdin);
-        strrtn(name);
-        printnpcc(name, name);
-        wait();
-
-        sprintf(buffer, "So %s... I have never seen you before...", name);
-        printnpcc("Bartender", buffer);
-        printnpcc("Bartender", "Where are you from?");
-
-        fgets(location, MAX_STRING_CHARS, stdin);
-        strrtn(location);
-        printnpcc(name, location);
-        wait();
-
-        sprintf(buffer, "Ah... %s... From afar I see...", location);
-        printnpcc("Bartender", buffer);
-        printnpcc("Bartender", "Make yourself at home, ask me if you need anything.");
-
-        printsep();
-        printf("---- What will you do? -----------------------------\n");
-        printf("[A] Grab a drink and sit at the bar.\n");
-        printf("[B] Grab a drink and sit at a empty table.\n");
-        printf("[C] Grab a drink and sit at a table beside someone.\n");
-        printsep();
-
-        init_choice();
-        
-        if (strcmp(choice, "A") == 0) {
-            printf("You order a drink and go sit at the bar.\n");
-            printf("After a short while, a stranger sits right next to you and orders a beer.\n");
-            printf("You notice his hand is torn open and bleeding.\n");
-            printnpcc("Bartender", "How are you going to pay for that beer, stranger?");
-            printf("It seems that the stranger does not have enough money to pay for the beer.\n");
-
-            printsep();
-            printf("---- What will you do? -----------------------------\n");
-            printf("[A] Pay for his beer.\n");
-            printf("[B] Don't pay for his beer.\n");
-            printsep();
-
-            init_choice();
-            
-            if (strcmp(choice, "A") == 0) {
-                printnpcc("Stranger", "Thank you Traveler...");
-            }
-        }
-    }*/
 }
